e3d4172d01b5bb8a3bb0e6eb03ba10399b928b1e
Rem <radrr@mail.ru>
2021-11-17 09:39:20 +0300

    add model, load json, make migration

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    photo = models.ImageField(verbose_name='Фото')
+    price = models.IntegerField(verbose_name='Цена')
+
