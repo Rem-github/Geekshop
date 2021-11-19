699e0aa29de5420b5bc158e6cc3873edb714792f
Rem <radrr@mail.ru>
2021-11-19 08:23:03 +0300

    add model Product and ProductCategory

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, verbose_name='Название')
+    description = models.TextField(blank=True, null=True)
 
-class Products(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название')
     description = models.TextField(blank=True, verbose_name='Описание')
-    photo = models.ImageField(verbose_name='Фото')
-    price = models.IntegerField(verbose_name='Цена')
-
+    photo = models.ImageField(upload_to='product_image', verbose_name='Фото')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey('ProductCategory', on_delete=models.CASCADE)
