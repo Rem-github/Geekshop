b589a7acc528de21beab785c145b98341841bf5a
Rem <radrr@mail.ru>
2021-11-19 20:47:10 +0300

    cosmetic refresh admin panel

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Product)
-admin.site.register(ProductCategory)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'photo', 'quantity', 'price')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'description')
+    list_filter = ('quantity', 'price')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductCategory, CategoryAdmin)
--- mainapp/models.py
+++ mainapp/models.py
@@ -6,14 +6,21 @@ class ProductCategory(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+    class Meta:
+        verbose_name = "Категории"
+        verbose_name_plural = "Категории"
 
 class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название')
     description = models.TextField(blank=True, verbose_name='Описание')
     photo = models.ImageField(upload_to='product_image', verbose_name='Фото')
     price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
-    quantity = models.PositiveIntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
     category = models.ForeignKey('ProductCategory', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name} | {self.category}'
+
+    class Meta:
+        verbose_name = "Продукты"
+        verbose_name_plural = "Продукты"
