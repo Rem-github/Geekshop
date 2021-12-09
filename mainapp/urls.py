6bc5cd7146203fac944f6ba8f4cc0ac447076a5c
Rem <radrr@mail.ru>
2021-12-09 19:43:14 +0300

    lesson 8 paginator

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,6 +7,7 @@ app_name = 'products'
 urlpatterns = [
     path('', products, name='products'),
     path('product/<int:id>/', product, name='product'),
-
+    path('category/<int:id_category>/', products, name='category'),
+    path('page/<int:page>/', products, name='page'),
 ]
 
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,10 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 
 # Create your views here.
-from mainapp.models import Product
+from mainapp.models import Product, ProductCategory
 
 
 def index(request):
@@ -12,12 +13,29 @@ def index(request):
     }
     return render(request, 'mainapp/index.html', context)
 
-def products(request):
-    products = Product.objects.all()
+def products(request, id_category=None, page=1):
+
     context = {
         'title': 'geekshop - Каталог',
-        'products': products,
     }
+
+    if id_category:
+        products=Product.objects.filter(category_id=id_category)
+    else:
+        products = Product.objects.all()
+
+    paginator=Paginator(products, per_page=3)
+
+    try:
+        products_paginator= paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator= paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+
+    context['products'] = products_paginator
+    context['categories']=ProductCategory.objects.all()
+
     return render(request, 'mainapp/products.html', context)
 
 def product(request, id):
